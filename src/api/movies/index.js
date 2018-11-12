74f0eeb61a07dde6fc12e80e81247e0bd4e76d10
Alber Erre <arblugo@gmail.com>
2018-11-12 17:19:29 +0000

    add magic refactor

--- src/api/movies/index.js
+++ src/api/movies/index.js
@@ -3,64 +3,10 @@ const _ = require("lodash");
 const router = express.Router();
 const magic = require(".whereMagicHappens");
 
-// Utils
-const bodyIsEmpty = require("../../utils/bodyIsEmpty");
-//const Movie = require("../../models/movie.model");
-const idExists = require("../../utils/idExists");
-
 router.get("/", (req, res) => magic.getMovies);
-
-router.get("/:id", (req, res) => {
-    movies.find(selectedMovie => {
-        if (selectedMovie.id == req.params.id) {
-            res.json(selectedMovie);
-        }
-    });
-
-    res.status(400).send(
-        `Oops, ID: ${req.params.id} hasn't have any movie assigned.`
-    );
-});
-
-router.post("/", (req, res) => {
-    if (!bodyIsEmpty(req.body)) {
-        res.status(400).send("Oops, you have to pass something as Body");
-    } else {
-        const newMovie = req.body;
-        newMovie.id = _.random(0, 100);
-        movies.push(newMovie);
-        //let newMovie = new Movie(_.random(0,100), ........);
-
-        res.json({
-            message: "New Movie added!",
-            newMovie
-        });
-    }
-});
-
-router.put("/:id", (req, res) => {
-    const newInfo = req.body;
-    let movieID = req.params.id;
-    let MovieToUpdate = movies.find(movie => movie.id == movieID);
-
-    res.json({
-        message: `Movie ${MovieToUpdate.id} has been updated.`,
-        MovieToUpdate
-    });
-});
-
-router.delete("/movies/:id", (req, res) => {
-    if (!idExists(movies, req.params.id)) {
-        res.status(400).send("Oops, that ID does not exist.");
-    } else {
-        movies.find(movieToDelete => {
-            if (movieToDelete.id == req.params.id) {
-                // remove from movies array (using lodash)
-                _.pull(movies, movieToDelete);
-                res.send(`Movie ${movieToDelete.id} has been deleted.`);
-            }
-        });
-    }
-});
+router.get("/:id", (req, res) => magic.getMovie);
+router.post("/", (req, res) => magic.addMovie);
+router.put("/:id", (req, res) => magic.updateMovie);
+router.delete("/movies/:id", (req, res) => magic.deleteMovie);
 
 module.exports = router;
--- src/api/movies/whereMagicHappens.js
+++ src/api/movies/whereMagicHappens.js
@@ -1,14 +1,66 @@
+// Utils
+const bodyIsEmpty = require("../../utils/bodyIsEmpty");
+const idExists = require("../../utils/idExists");
+
 // Movies Data
 let movies = require("../../data/movies");
 
-function getMovies() {
+// Movies API Methods
+function getMovies(req, res) {
     res.json(movies);
 }
 
-function getMovie() {}
+function getMovie(req, res) {
+    movies.find(selectedMovie => {
+        if (selectedMovie.id == req.params.id) {
+            res.json(selectedMovie);
+        }
+    });
 
-function updateMovie() {}
+    res.status(400).send(
+        `Oops, ID: ${req.params.id} hasn't have any movie assigned.`
+    );
+}
 
-function deleteMovie() {}
+function addMovie(req, res) {
+    if (!bodyIsEmpty(req.body)) {
+        res.status(400).send("Oops, you have to pass something as Body");
+    } else {
+        const newMovie = req.body;
+        newMovie.id = _.random(0, 100);
+        movies.push(newMovie);
+        //let newMovie = new Movie(_.random(0,100), ........);
 
-module.exports = { getMovie, getMovies, updateMovie, deleteMovie };
+        res.json({
+            message: "New Movie added!",
+            newMovie
+        });
+    }
+}
+
+function updateMovie(req, res) {
+    const newInfo = req.body;
+    let movieID = req.params.id;
+    let MovieToUpdate = movies.find(movie => movie.id == movieID);
+
+    res.json({
+        message: `Movie ${MovieToUpdate.id} has been updated.`,
+        MovieToUpdate
+    });
+}
+
+function deleteMovie(req, res) {
+    if (!idExists(movies, req.params.id)) {
+        res.status(400).send("Oops, that ID does not exist.");
+    } else {
+        movies.find(movieToDelete => {
+            if (movieToDelete.id == req.params.id) {
+                // remove from movies array (using lodash)
+                _.pull(movies, movieToDelete);
+                res.send(`Movie ${movieToDelete.id} has been deleted.`);
+            }
+        });
+    }
+}
+
+module.exports = { getMovie, getMovies, addMovie, updateMovie, deleteMovie };
