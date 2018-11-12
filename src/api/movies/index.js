c62f1124bd22102d04a2339ba365543274cf4477
Alber Erre <arblugo@gmail.com>
2018-11-12 17:45:11 +0000

    rm movies typo

--- src/api/movies/index.js
+++ src/api/movies/index.js
@@ -7,6 +7,6 @@ router.get("/", (req, res) => magic.getMovies(req, res));
 router.get("/:id", (req, res) => magic.getMovie(req, res));
 router.post("/", (req, res) => magic.addMovie(req, res));
 router.put("/:id", (req, res) => magic.updateMovie(req, res));
-router.delete("/movies/:id", (req, res) => magic.deleteMovie(req, res));
+router.delete("/:id", (req, res) => magic.deleteMovie(req, res));
 
 module.exports = router;
