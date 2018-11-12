f2aae9ab1578126ff122b8830757b2fa964517ad
Alber Erre <arblugo@gmail.com>
2018-11-12 13:59:20 +0000

    add post method

--- src/server.js
+++ src/server.js
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
     res.json(movies);
 });
 
+app.post("/movies", (req, res) => {
+    if (bodyIsEmpty(req.body)) {
+        res.status(400).send("Oops, you have to pass something as Body");
+    } else {
+        const newMovie = req.body;
+        newMovie.id = _.random(0,100);
+        movies.push(newMovie);
+        //let newMovie = new Movie(_.random(0,100), ........);
+
+        res.json({
+            message: 'New Movie added!',
+            newMovie
+        });
+    }
+});
+
 // Listen
 app.listen(port, () => {
     console.log("Express running on port 3001.");
---
+++ src/utils/bodyIsEmpty.js
@@ -0,0 +1,13 @@
+function bodyIsEmpty(body) {
+    if (
+        (body !== null) |
+        (body !== undefined) |
+        (Object.keys(body).length > 0)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = { bodyIsEmpty };
