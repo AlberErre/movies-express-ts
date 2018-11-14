27102dad19a65a8df73b01a8e574382626c916e2
Alber Erre <arblugo@gmail.com>
2018-11-14 16:49:42 +0000

    ts-update

---
+++ src/data/movies.ts
@@ -0,0 +1,55 @@
+import { Movie } from '../models/model';
+
+export const movies: Movie[] = [
+  {
+    id: '0',
+    name: 'Harry Potter and the Prisoner of Azkaban',
+    year: 2004,
+    genre: 'fantasy',
+    director: 'Alfonso Cuaron',
+    description: 'It\'s Harry\'s third year at Hogwarts; ' +
+      'not only does he have a new Defense Against the Dark ' +
+      'Arts teacher, but there is also trouble brewing. Convicted ' +
+      'murderer Sirius Black has escaped the Wizards\' Prison and is ' +
+      'coming after Harry.',
+    nsfw: false,
+    like: false
+  },
+  {
+    id: '1',
+    name: 'Avatar',
+    year: 2009,
+    genre: 'fantasy',
+    director: 'James Cameron',
+    description: 'A paraplegic marine dispatched to the moon' +
+      ' Pandora on a unique mission becomes torn between following ' +
+      'his orders and protecting the world he feels is his home.',
+    nsfw: false,
+    like: false
+  },
+  {
+    id: '2',
+    name: 'Interstellar',
+    year: 2014,
+    genre: 'science',
+    director: 'Christopher Nolan',
+    description: 'Dystopian future where humanity is struggling to' +
+      ' survive, the film follows a group of astronauts who travel ' +
+      'through a wormhole in search of a new home for humanity.',
+    nsfw: false,
+    like: false
+  },
+  {
+    id: '3',
+    name: 'Inside Out',
+    year: 2015,
+    genre: 'animation',
+    director: 'Pete Docter',
+    description: 'After young Riley is uprooted from her Midwest life ' +
+      'and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust ' +
+      'and Sadness - conflict on how best to navigate a new city, house, ' +
+      'and school.',
+    nsfw: false,
+    like: false
+  }
+];
---
+++ src/models/model.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+  id: string;
+  name: string;
+  year: number;
+  genre: string;
+  director: string;
+  description: string;
+  nsfw: boolean;
+  like: boolean;
+}
