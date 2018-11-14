c79015428eaceaf0d2d14b085bf8c25819f7ad45
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-14 16:39:54 +0000

    Added config and package.json changes

--- src/models/movie.model.js
+++ src/models/movie.model.js
@@ -1,20 +1,19 @@
 class Movie {
-    constructor(id = "none",
-                name = "First movie ever",
-                year = "1895", 
-                genre = "vintage",
-                director = "Lumiere brothers",
-                description = "First movie in history, created by Lumiere brothers",
-                nsfw = false) {
+  constructor(id = "none",
+              name = "First movie ever",
+              year = "1895", 
+              genre = "vintage",
+              director = "Lumiere brothers",
+              description = "First movie in history, created by Lumiere brothers",
+              nsfw = false) {
 
-      this.id = id;
-      this.name = name;
-      this.year = year;
-      this.genre = genre;
-      this.director = director;
-      this.description = description;
-      this.nsfw = nsfw;
-    }
+    this.id = id;
+    this.name = name;
+    this.year = year;
+    this.genre = genre;
+    this.director = director;
+    this.description = description;
+    this.nsfw = nsfw;
   }
-  module.exports = { Movie };
-  
+}
+module.exports = { Movie };
